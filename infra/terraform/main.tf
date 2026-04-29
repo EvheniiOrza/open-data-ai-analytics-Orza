7c911c60b2023bbb39874c64dac462c032ba6e16
Evhenii <yevhenii.orza.shi.2023@lpnu.ua>
2026-04-29 21:04:27 +0300

    lab4

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
   }
 
   os_disk {
